dc4bf39834554634f7bb5776a3766d59a90b7cc5
SameerJadav <sameerjadav010@gmail.com>
2023-06-27 10:48:18 +0530

    site config

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: 'Sameer Jadav',
+  description:
+    "I'm a full stack developer based in India. I like creative coding, VFX, and CGI. Let's connect and see where it takes us.",
+  url: 'https://sameerjadav.me',
+  ogImage: 'https://sameerjadav.me/og.png',
+  author: 'Sameer Jadav',
+  twitterId: '@SameerJadav_',
+  links: {
+    twitter: 'https://twitter.com/SameerJadav_',
+    github: 'https://github.com/SameerJadav',
+    linkedin: 'https://www.linkedin.com/in/sameer-jadav',
+    instagram: 'https://www.instagram.com/sameerjadav._',
+    mail: 'mailto:sameerjdav001@gmail.com',
+  },
+};
