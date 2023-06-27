cc4c4900bc53696a55f25220fb8ae8964f71675b
SameerJadav <sameerjadav010@gmail.com>
2023-06-27 13:20:26 +0530

    content for "about" section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hellow world!</h1>
+      <p className="mt-6 md:mt-8">
+        Hey there! I&apos;m Sameer Jadav, a 19 y/o from India. I like coding,
+        VFX, and CGI. I&apos;ve got a real passion for this thing called
+        creative coding—it&apos;s like blending art and tech, it&apos;s awesome!
+        I also enjoy exploring and figuring things out about myself.
+      </p>
+      <p className="mt-4">
+        Not too long ago, I used to spend countless hours watching anime. Then
+        one day my friend called me to let me know he was getting himself a
+        fancy new laptop! That got me thinking, &quot;Hey, dude, can I have your
+        old laptop?&quot; And guess what? He was kind enough to say yes!
+      </p>
+      <p className="mt-4">
+        With this shiny new piece of tech, I decided to ditch the anime binges
+        and dive into learning some serious skills. And guess what I stumbled
+        upon? Programming, specifically web development. Maybe it&apos;s because
+        I started with HTML and CSS—I just loved seeing my creations come to
+        life right there in the browser. Now, I&apos;m on a mission to create
+        stuff that&apos;s not only impressive but also brings a whole lot of
+        joy.
+      </p>
+      <p className="mt-4">
+        I may not be the most outgoing person, but I&apos;m always up for making
+        new friends and having a good chat. Let&apos;s connect and see where it
+        takes us. And hey, if you&apos;re looking for someone with my skills,
+        check out my resume—I&apos;m currently seeking job or internship
+        opportunities.
+      </p>
     </main>
   )
 }
