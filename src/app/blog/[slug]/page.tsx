4ccbd92ce6721c51f129b3d50578c1306214d0cf
SameerJadav <sameerjadav010@gmail.com>
2023-07-29 01:19:27 +0530

    post page and rough notation

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,90 @@
+import { type Metadata } from "next"
+import { notFound } from "next/navigation"
+import { allPosts } from "contentlayer/generated"
+import { Balancer } from "react-wrap-balancer"
+import { SITE } from "~/config"
+import { formatDate } from "~/lib/utils"
+import { MyLink } from "~/components/MyLink"
+
+interface PostPageProps {
+  params: {
+    slug: string
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slugAsParams,
+  }))
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+async function getPostFromParams({ params }: PostPageProps) {
+  const slug = params.slug
+
+  const post = allPosts.find((post) => post.slugAsParams === slug)
+
+  if (!post) null
+
+  return post
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const post = await getPostFromParams({ params })
+
+  if (!post) notFound()
+
+  const title = post.title
+  const description = post.description
+  const url = `${SITE.url}/blog/${post.slugAsParams}`
+  const image = `${SITE.image}/og?title=${title}`
+
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      type: "article",
+      title: title,
+      description: description,
+      url: url,
+      images: [
+        {
+          url: image,
+          width: 1200,
+          height: 630,
+          alt: SITE.name,
+        },
+      ],
+    },
+    twitter: {
+      title: title,
+      description: description,
+      images: [image],
+    },
+    alternates: { canonical: url },
+  }
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const post = await getPostFromParams({ params })
+
+  if (!post) notFound()
+
+  // const Content = getMDXComponent(post.body.code)
+
+  return (
+    <article>
+      <h1 className="mt-8 text-2xl font-bold tracking-tighter">
+        <Balancer>{post.title}</Balancer>
+      </h1>
+      <div className="flex items-center justify-between text-sm">
+        <p className="font-mono text-slate11">{formatDate(post.publishedAt)}</p>
+        <MyLink href="/blog" title="Blog" />
+      </div>
+    </article>
+  )
+}
--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,33 +1,47 @@
 "use client"
 
+import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { cn } from "~/lib/utils"
+import { RoughNotation } from "react-rough-notation"
+
+const home = (pathname: string) => {
+  return pathname === "/" ? (
+    <RoughNotation type="underline" animate color="#4c5155" show padding={6}>
+      Home
+    </RoughNotation>
+  ) : (
+    <Link
+      href="/"
+      className="text-slate11 transition-colors duration-200 ease-in hover:text-slate12 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate6"
+    >
+      Home
+    </Link>
+  )
+}
+
+const blog = (pathname: string) => {
+  return pathname === "/blog" ? (
+    <RoughNotation type="underline" animate color="#4c5155" show padding={6}>
+      Blog
+    </RoughNotation>
+  ) : (
+    <Link
+      href="/blog"
+      className="text-slate11 transition-colors duration-200 ease-in hover:text-slate12 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate6"
+    >
+      Blog
+    </Link>
+  )
+}
 
 export default function SiteHeader() {
-  const pathname = usePathname()
+  let pathname = usePathname()
+  if (pathname.includes("/blog/")) pathname = "/blog"
   return (
     <header className="mt-8 w-full md:mt-16">
       <nav className="flex items-center gap-4">
-        <a
-          href="/"
-          className={cn(
-            "text-slate11 transition-colors duration-200 ease-in hover:text-slate12",
-            pathname === "/" &&
-              "text-slate12 underline decoration-slate7 underline-offset-8 hover:decoration-slate8",
-          )}
-        >
-          Home
-        </a>
-        <a
-          href="/blog"
-          className={cn(
-            "text-slate11 transition-colors duration-200 ease-in hover:text-slate12",
-            pathname === "/blog" &&
-              "text-slate12 underline decoration-slate7 underline-offset-8 hover:decoration-slate8",
-          )}
-        >
-          Blog
-        </a>
+        {home(pathname)}
+        {blog(pathname)}
       </nav>
     </header>
   )
