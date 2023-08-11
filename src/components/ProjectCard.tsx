e9a484baad9076db14e0e316836f2c37f653e3ea
SameerJadav <sameerjadav010@gmail.com>
2023-08-11 12:06:07 +0530

    made project card responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -35,7 +35,7 @@ export default function ProjectCard({
   direction,
 }: ProjectCardProps) {
   return direction === "left" ? (
-    <div className="flex flex-col first:pb-6 md:flex-row md:divide-x md:divide-dashed md:divide-gray6 md:[&:not(:first-child)]:pt-6">
+    <div className="flex flex-col first:pb-6 only:p-0 md:flex-row md:divide-x md:divide-dashed md:divide-gray6 [&:not(:first-child)]:pt-6">
       <div className="flex-1 md:pr-6">
         <Image
           src={image}
@@ -44,18 +44,18 @@ export default function ProjectCard({
           className="rounded-md border border-gray6"
         />
       </div>
-      <div className="mt-6 flex-1 md:mt-0 md:pl-6 md:text-left">
+      <div className="mt-4 flex-1 md:mt-0 md:pl-6 md:text-left">
         <ProjectLink href={href} title={title} />
-        <p className="mt-4 text-lg text-gray11">{description}</p>
+        <p className="mt-2 text-lg text-gray11">{description}</p>
       </div>
     </div>
   ) : (
-    <div className="flex divide-x divide-dashed divide-gray6 first:pb-6 [&:not(:first-child)]:pt-6">
-      <div className="flex-1 pr-6 md:text-right">
+    <div className="flex flex-col-reverse first:pb-6 only:p-0 md:flex-row md:divide-x md:divide-dashed md:divide-gray6 [&:not(:first-child)]:pt-6">
+      <div className="mt-4 flex-1 md:mt-0 md:pr-6 md:text-right">
         <ProjectLink href={href} title={title} />
-        <p className="mt-4 text-lg text-gray11">{description}</p>
+        <p className="mt-2 text-lg text-gray11">{description}</p>
       </div>
-      <div className="flex-1 pl-6">
+      <div className="flex-1 md:pl-6">
         <Image
           src={image}
           alt={imageAlt}
