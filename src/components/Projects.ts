a8aeb472c6ce3f72d074ee458d14ef5c27ec685b
SameerJadav <sameerjadav010@gmail.com>
2023-08-11 18:42:31 +0530

    added my website as project

--- src/components/Projects.ts
+++ src/components/Projects.ts
@@ -1,5 +1,6 @@
 import { type StaticImageData } from "next/image"
 import guestbook from "~/images/guestbook.png"
+import website from "~/images/website.png"
 
 interface Project {
   id: number
@@ -14,12 +15,22 @@ interface Project {
 export const projects: Project[] = [
   {
     id: 1,
-    href: "https://guestbook.sameerjadav.me",
     title: "Guestbook",
     description:
       "A simple and user-friendly full-stack guestbook application built with Next.js and serverless technologies bootstrapped with `create t3-app`.",
+    href: "https://guestbook.sameerjadav.me",
     image: guestbook,
     imageAlt: "Guestbook app screenshot",
     direction: "left",
+  },
+  {
+    id: 2,
+    title: "sameerjadav.me",
+    description:
+      "My website, built with Next.js, Tailwind CSS, and Contentlayer, is feature-rich and includes a stunning blog, engaging comment section, and like functionality.",
+    href: "https://sameerjadav.me",
+    image: website,
+    imageAlt: "My website screenshot",
+    direction: "right",
   },
 ]
