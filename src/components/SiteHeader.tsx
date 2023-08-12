67d84cd50b7ee7574f10a6e4ea752ac3de30d967
SameerJadav <sameerjadav010@gmail.com>
2023-08-12 16:43:33 +0530

    animated header

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,19 +1,63 @@
 "use client"
 
+import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { blog, home, notes } from "~/components/NavItems"
+import { LayoutGroup, motion } from "framer-motion"
+import { cn } from "~/lib/utils"
+
+const navItems = {
+  "/": {
+    lable: "Home",
+  },
+  "/blog": {
+    lable: "Blog",
+  },
+  "/notes": {
+    lable: "Notes",
+  },
+}
 
 export default function SiteHeader() {
   let pathname = usePathname()
   if (pathname.includes("/blog/")) pathname = "/blog"
   if (pathname.includes("/notes/")) pathname = "/notes"
+
   return (
     <header className="mt-8 w-full md:mt-16">
       <nav>
         <ul className="flex items-center gap-4">
-          <li>{home(pathname)}</li>
-          <li>{blog(pathname)}</li>
-          <li>{notes(pathname)}</li>
+          <LayoutGroup>
+            {Object.entries(navItems).map(([path, { lable }]) => {
+              const isActive = pathname === path
+              return (
+                <li key={path} className="font-medium">
+                  <Link
+                    href={path}
+                    className={cn(
+                      "text-gray11 transition-colors ease-in hover:text-gray12 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray7",
+                      isActive && "pointer-events-none text-gray12",
+                    )}
+                  >
+                    <div className="relative px-2 py-1">
+                      <span>{lable}</span>
+                      {isActive ? (
+                        <motion.div
+                          className="absolute inset-0 top-7 mx-2 h-0.5 rounded-full bg-gradient-to-r from-gray8 to-gray1"
+                          layoutId="sidebar"
+                          transition={{
+                            type: "spring",
+                            stiffness: 350,
+                            damping: 30,
+                            duration: 150,
+                          }}
+                        />
+                      ) : null}
+                    </div>
+                  </Link>
+                </li>
+              )
+            })}
+          </LayoutGroup>
         </ul>
       </nav>
     </header>
