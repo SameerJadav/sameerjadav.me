8a9fbe1def84d37adc2bd70f53107a2006dc77e1
SameerJadav <sameerjadav010@gmail.com>
2023-12-03 11:19:56 +0530

    refactor syntax highlighting

--- src/components/Mdx.tsx
+++ src/components/Mdx.tsx
@@ -15,6 +15,12 @@ interface CalloutProps {
 const rehypePrettyCodeOptions: Options = {
   theme: "one-dark-pro",
   keepBackground: false,
+  tokensMap: {
+    // VScode command palette: Inspect Editor Tokens and Scopes
+    // https://github.com/Binaryify/OneDark-Pro/blob/47c66a2f2d3e5c85490e1aaad96f5fab3293b091/themes/OneDark-Pro.json
+    fn: "entity.name.function",
+    objKey: "meta.object-literal.key",
+  },
 };
 
 function Callout({ emoji, children }: CalloutProps) {
