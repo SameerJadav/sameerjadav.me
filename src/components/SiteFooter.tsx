2d0aa947f49ed3b9520341c62f7d30089e9391b7
SameerJadav <sameerjadav010@gmail.com>
2024-03-01 21:02:12 +0530

    refactor: update social links order in footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -7,21 +7,21 @@ export default function SiteFooter() {
     <footer className="my-8 flex items-center justify-between gap-4 border-t border-gray-6 pt-6">
       <p className="text-lg font-medium">{SITE.name}</p>
       <div className="flex items-center gap-4">
-        <ExternalLink highlight url={SITE.links.twitter}>
-          <Icons.Twitter className="size-6" />
-          <span className="sr-only">Link to my Twitter</span>
-        </ExternalLink>
         <ExternalLink highlight url={SITE.links.github}>
           <Icons.Github className="size-6" />
           <span className="sr-only">Link to my GitHub</span>
+        </ExternalLink>
+        <ExternalLink highlight url={SITE.links.mail}>
+          <Icons.Mail className="size-6" />
+          <span className="sr-only">Link to my E-Mail</span>
         </ExternalLink>
         <ExternalLink highlight url={SITE.links.linkedin}>
           <Icons.Linkedin className="size-6" />
           <span className="sr-only">Link to my LinkedIn</span>
         </ExternalLink>
-        <ExternalLink highlight url={SITE.links.mail}>
-          <Icons.Mail className="size-6" />
-          <span className="sr-only">Link to my E-Mail</span>
+        <ExternalLink highlight url={SITE.links.twitter}>
+          <Icons.Twitter className="size-6" />
+          <span className="sr-only">Link to my Twitter</span>
         </ExternalLink>
       </div>
     </footer>
